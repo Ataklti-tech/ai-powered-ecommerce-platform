1dba6cdbc3e00a9455e826d909b95f7955f7293a
Ataklti Okbe <ae149964@students.cavendish.ac.ug>
2026-02-06 14:47:37 +0300

    Update the Header directory import or module directory in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "./Header";
+import Header from "././components/common/Header/Header";
 import { BrowserRouter as Router } from "react-router-dom";
 // import FeaturedProducts from "./pages/Home/FeaturedProducts";
 
