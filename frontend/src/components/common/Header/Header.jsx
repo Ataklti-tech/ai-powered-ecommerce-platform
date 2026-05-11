8f3085f4c875f7ec2501b039365a72994d86abc2
Ataklti Okbe <ae149964@students.cavendish.ac.ug>
2026-05-11 10:16:31 +0300

    style(Header): rename brand text from PickPerfect to Agelgil

--- frontend/src/components/common/Header/Header.jsx
+++ frontend/src/components/common/Header/Header.jsx
@@ -98,7 +98,7 @@ const Header = ({
         <div className="header__logo">
           <Link to="/" className="header__logo-link">
             <ShoppingCart className="header__logo-icon" />
-            <span className="header__logo-text">PickPerfect</span>
+            <span className="header__logo-text">Agelgil</span>
           </Link>
         </div>
 
